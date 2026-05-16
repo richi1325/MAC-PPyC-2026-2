2c9f6d16d93366076a41c6b2496b2f788c5d3e97
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2026-05-16 01:30:51 -0600

    Feat: fixing memoization code

--- Clase 6/meoization.py
+++ Clase 6/meoization.py
@@ -1,7 +1,12 @@
->>> def f(n, memoria = {}):
-...     if n in (1,0):
-...         return 1
-...     if n not in memoria:
-...         memoria[n] = f(n-1) + f(n-2)
-...     return memoria[n]
-...
+import sys
+sys.setrecursionlimit(50000)
+
+
+def f(n, memoria = {}):
+    if n in (1,0):
+        return 1
+    if n not in memoria:
+        memoria[n] = f(n-1) + f(n-2)
+    return memoria[n]
+
+print(f(10000))
